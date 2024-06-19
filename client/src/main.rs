35fecb951af343fefa5265903d53e35d6ef1b45f
iLeanna0 <125588588+iLeanna0@users.noreply.github.com>
2024-06-20 00:25:18 +0200

    file Serializing and Deserializing

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,28 +1,10 @@
-// A command line chat client will probably need some styling / high-level interaction with the command line. So this
-// project includes `crossterm` by default, a Rust library for cross-plattform command line manipulation
-
-// use crossterm::{
-//     style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
-//     ExecutableCommand,
-// };
-
-// fn main() {
-//     // crossterm commands can fail. For now, you are allowed to use `.unwrap()`, but you can also take a look at error
-//     // handling in Rust and do it right from the start. We will talk about error handling probably towards the end of
-//     // May
-
-//     std::io::stdout()
-//         .execute(SetForegroundColor(Color::Blue))
-//         .unwrap()
-//         .execute(SetBackgroundColor(Color::Red))
-//         .unwrap()
-//         .execute(Print("Styled text here."))
-//         .unwrap()
-//         .execute(ResetColor)
-//         .unwrap();
-// }
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
 
 use async_std::{
+    fs::File,
     io::{stdin, BufReader},
     net::{TcpStream, ToSocketAddrs},
     prelude::*,
@@ -31,6 +13,12 @@ use async_std::{
 use futures::{select, FutureExt};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct FileTransfer {
+    filename: String,
+    data: Vec<u8>,
+}
 
 // main
 fn main() -> Result<()> {
@@ -42,24 +30,60 @@ async fn try_run(addr: impl ToSocketAddrs) -> Result<()> {
     let (reader, mut writer) = (&stream, &stream); // 1
     let mut lines_from_server = BufReader::new(reader).lines().fuse(); // 2
     let mut lines_from_stdin = BufReader::new(stdin()).lines().fuse(); // 2
+    
     loop {
         select! { // 3
             line = lines_from_server.next().fuse() => match line {
                 Some(line) => {
                     let line = line?;
                     println!("{}", line);
+
+                    // Deserialize if it's a file transfer
+                    if let Ok(file_transfer) = serde_json::from_str::<FileTransfer>(&line) {
+                        save_to_file(&file_transfer).await?;
+                        println!("File {} saved.", file_transfer.filename);
+                    }
                 },
                 None => break,
             },
             line = lines_from_stdin.next().fuse() => match line {
                 Some(line) => {
                     let line = line?;
-                    writer.write_all(line.as_bytes()).await?;
-                    writer.write_all(b"\n").await?;
+                    if line.starts_with("file:") {
+                        let filename = line.trim_start_matches("file:").trim();
+                        send_file(&filename, &mut writer.clone()).await?;
+                    } else {
+                        writer.write_all(line.as_bytes()).await?;
+                        writer.write_all(b"\n").await?;
+                    }
                 }
                 None => break,
             }
         }
     }
+    Ok(())
+}
+
+
+async fn send_file(filename: &str, writer: &mut TcpStream) -> Result<()> {
+    let mut file = File::open(filename).await?;
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).await?;
+
+    let file_transfer = FileTransfer {
+        filename: filename.to_string(),
+        data: buffer,
+    };
+
+    let serialized = serde_json::to_string(&file_transfer)?;
+    writer.write_all(serialized.as_bytes()).await?;
+    writer.write_all(b"\n").await?;
+    println!("File {} sent.", filename);
+    Ok(())
+}
+
+async fn save_to_file(file_transfer: &FileTransfer) -> Result<()> {
+    let mut file = File::create(&file_transfer.filename).await?;
+    file.write_all(&file_transfer.data).await?;
     Ok(())
 }
